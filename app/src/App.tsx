7b1bd213b1da99bc6812eb247f81c711c87f1469
Ondřej Hlaváč <ondrej.hlavac@outlook.com>
2023-04-28 16:41:31 +0200

    init styled components

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,14 @@
 import { useState } from "react";
+import { Headline } from "./styled";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <>app</>;
+  return (
+    <>
+      <Headline>Kurzovní lístek</Headline>
+    </>
+  );
 }
 
 export default App;
